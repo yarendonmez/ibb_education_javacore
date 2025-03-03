84f5c8c89b41682eef7913acdaaaa079a4e5e2b9
yarendonmez <yarendonmez034@gmail.com>
2025-03-03 04:34:16 +0300

    SumUntil100

--- src/main/java/com/yarendonmez/Examples/SumUntil100.java
+++ src/main/java/com/yarendonmez/Examples/SumUntil100.java
@@ -3,27 +3,34 @@ package com.yarendonmez.Examples;
 import java.util.Scanner;
 
 public class SumUntil100 {
+    /* 1.  Kullanıcının Verdiği pozitif bir sayıya kadar toplamını yapan hesaplayan Algoritma ?
+     Şartlar - 1 : Kullanıcı eğer sayı olarak  100  fazla sayı girerse 100 kadar toplasın. (break)
+     Şartlar - 2 : Kullanıcı verdiği sayılar içinde eğer 47 varsa bunu toplamaya dahil etmesin.(continue)
+     Soru:
+     Çözüm:
+     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("1 den x e kadar olan sayıların toplamı için x değerini giriniz: ");
-
-        int number = input.nextInt(); //Kullanıcıdan sayı alma
+        System.out.println("Pozitif bir sayı giriniz ");
+        int number = Math.abs(input.nextInt());//Kullanıcıdan sayı alma
         int sum = 0;
-        int i = 0;
 
-        if (number > 100)
-            System.out.println("100'den küçük bir sayı giriniz");
-
-        else {
-            while (i <= number) {
-                if (i == 47) {
-                    continue;
-                }
-                sum = sum + i;
-                i++;
+        for (int i = 1; i <= number; i++) {
+            if (i == 47) {
+                System.out.println("47 sayısı toplama dışında tutulacaktır");
+                continue;//47 yi atla
             }
-        }
-        System.out.println("Sonuç: " + sum);
 
+
+            //100 den fazla veri olursa
+            if (i > 100) {
+                System.out.println("Sayı üst sınırı olan 100'ü aştınız!");
+                break;
+            }
+            sum += i;
+        }
+        System.out.println("Sonuç: "+sum);
     }
 }
+
+
