c428c8c63734ac08c2a3790047ba55a02a765737
yarendonmez <yarendonmez034@gmail.com>
2025-03-10 02:45:19 +0300

    condational update

--- src/main/java/com/yarendonmez/_2_week/_09_01_Condational.java
+++ src/main/java/com/yarendonmez/_2_week/_09_01_Condational.java
@@ -2,9 +2,11 @@ package com.yarendonmez._2_week;
 
 public class _09_01_Condational {
     public static void main(String[] args) {
-    int num = 5;
-
-    String result = (num < 0) ? num + " negatiftir" : num + " pozitiftir";
-    System.out.println(result);
-}
+        int number = 5;
+        if (number > 0) {
+            System.out.println("Pozitif sayıdır");
+        } else {
+            System.out.println("Negatif Sayıdır");
+        }
+    }
 }
