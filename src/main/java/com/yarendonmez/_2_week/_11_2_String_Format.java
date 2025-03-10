bbc781eda569c43101bbe5496e44febc15453b82
yarendonmez <yarendonmez034@gmail.com>
2025-03-11 02:22:36 +0300

    formatter.closee

--- src/main/java/com/yarendonmez/_2_week/_11_2_String_Format.java
+++ src/main/java/com/yarendonmez/_2_week/_11_2_String_Format.java
@@ -7,6 +7,7 @@ public class _11_2_String_Format {
         Formatter formatter = new Formatter();
         formatter.format("Merhabalar adınız: %s, soyadınız: %s, yaşınız: %d, maaşınız: %f", "Yaren", "Dönmez", 23, 136.9);
         System.out.println(formatter);
+        formatter.close(); //Belleği serbest bırakma
     }
 
     public static void main(String[] args) {
