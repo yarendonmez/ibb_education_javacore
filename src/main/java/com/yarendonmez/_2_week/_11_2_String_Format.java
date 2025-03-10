03b404aaa22373932d62153f8a03e3db3c06b1c6
yarendonmez <yarendonmez034@gmail.com>
2025-03-11 02:09:34 +0300

    Formatter

---
+++ src/main/java/com/yarendonmez/_2_week/_11_2_String_Format.java
@@ -0,0 +1,15 @@
+package com.yarendonmez._2_week;
+
+import java.util.Formatter;
+
+public class _11_2_String_Format {
+    public static void formatter1() {
+        Formatter formatter = new Formatter();
+        formatter.format("Merhabalar adınız: %s, soyadınız: %s, yaşınız: %d, maaşınız: %f", "Yaren", "Dönmez", 23, 136.9);
+        System.out.println(formatter);
+    }
+
+    public static void main(String[] args) {
+        formatter1();
+    }
+}
